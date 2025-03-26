9b7182e5aea752099c8e46ad321d67d397738532
Mussharma013 <013muskansharma@gmail.com>
2025-03-26 12:18:00 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Programmer","Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  // Navigation menu toggle
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".navbar");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
+
+// Scroll to section
+const links = document.querySelectorAll(".navbar a");
+
+links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const id = link.getAttribute("href");
+        const section = document.querySelector(id);
+        section.scrollIntoView({ behavior: "smooth" });
+    });
+});
+// Active link
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (window.scrollY >= sectionTop - 10) {
+            current = section.getAttribute("id");
+        }
+    });
+    links.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+// Scroll reveal animation
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 1000,
+    delay: 500,
+});
+
+scrollReveal.reveal(".home-content h3, .home-content h1, .home-content p, .home-sci a, .btn-box", {
+    interval: 500,
+});
+
+  
